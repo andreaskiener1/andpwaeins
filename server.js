8a32786543c6fd5e7828360d8aab1adfa6d58a04
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-06-14 18:22:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
+
+var express = require('express')
+var path = require('path')
+
+var app = express()
+app.set('port', (process.env.PORT || 5000))
+
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname)));
+
+var dateipfad = path.join(__dirname, 'public', 'index.html')
+ 
+app.get('/', function(request, response) {
+    response.sendFile(dateipfad)
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+/*
 const express = require('express');
 const app = express();
 
@@ -11,3 +32,4 @@ const server = app.listen(8081, () => {
 
   console.log('App listening at http://%s:%s', host, port);
 });
+*/
